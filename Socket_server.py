9bc1cc046a6157584969d911351036a8c740ec8e
varunbajpai <35426055+varunbajpai@users.noreply.github.com>
2018-04-03 12:25:10 +0530

    Add files via upload

--- Socket_server.py
+++ Socket_server.py
@@ -1,20 +1,23 @@
 import time
 import socket 
+import threading
 
 s = socket.socket()         
 port = 12345 
 s.bind(('', port))        
-s.listen(5)     
+s.listen(5)
 
-c, addr = s.accept()     
-print('accepted',addr)
-c.send('Thank you for connecting'.encode('utf-8'))
+def new_thread(c):
+	while True:
+		val = c.recv(1024)
+		c.send(val)
+		print(val)
 
 while True:
-	val = c.recv(1024)
-	c.send(val)
-	print(val)
-
+	c, addr = s.accept()     
+	print('accepted',addr)
+	c.send('Thank you for connecting'.encode('utf-8'))
+	t1 = threading.Thread(target=new_thread,args=(c,))
+	t1.start()
 		
-c.close()
    
